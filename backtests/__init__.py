25699f5cc2ac5cbb0a8abcf7726af05f34cf6f8b
Bruno de Oliveira <badeoliveirag@gmail.com>
2025-03-23 22:41:55 +0000

    fix import on backtests

---
+++ backtests/__init__.py
@@ -0,0 +1,9 @@
+"""
+Backtests Package
+
+This package contains backtesting utilities and strategy implementations.
+"""
+
+from backtests.utils.backtest_functions import BaseStrategy
+
+__all__ = ['BaseStrategy'] 
---
+++ backtests/utils/__init__.py
@@ -0,0 +1,9 @@
+"""
+Backtest Utilities Package
+
+This package contains utility functions and classes for backtesting.
+"""
+
+from backtests.utils.backtest_functions import BaseStrategy
+
+__all__ = ['BaseStrategy'] 
